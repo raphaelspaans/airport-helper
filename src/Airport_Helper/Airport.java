552a37cc56a312d0a1cd0e97848f8f83ef753cd8
MaxHilt <vhilten.m@gmail.com>
2016-02-24 11:23:44 +0100

    Update Airport.java

--- src/Airport_Helper/Airport.java
+++ src/Airport_Helper/Airport.java
@@ -1,4 +1,31 @@
 
+//package airport;
+
 public class Airport {
-  
+
+	public String name;
+	public String location;
+	public String shortcut; // the shortcut code for an airport. e.g. AMS for amsterdam
+	public String checkin;     // the area where passengers can check in for their flights. Needs to be connected to flight. 
+	public int Passengers;         // the program must calculate the amount of passengers - per airport.
+	
+	Airport (String Airportname, String AirportLocation, String AirportShortCut, String CheckInDesk, int PassengerCount)
+	{
+		// object airport constructs 
+		name = Airportname;
+		location = AirportLocation;
+		shortcut = AirportShortCut;
+		checkin = CheckInDesk;
+		Passengers = PassengerCount; 
+	}
+		
+
+	public static void countUsagePerPassenger ()   //void for now to not get an error 
+	{
+		int passengerCount = 0; 
+	}
+	
+	//return count; 
+	
+
 }
